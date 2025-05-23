0dd9df1ec58414fecd343ff5c744126f4de0f725
fournest sebastien <sebfour85@gmail.com>
2025-05-23 13:28:32 +0200

    exercice 1 fini

---
+++ script-exercice1.js
@@ -0,0 +1,9 @@
+// exercice 1
+
+
+let element = document.getElementById(`titre`);
+element.textContent = `Bonjour étudiant!`;
+
+let element2 = document.getElementById(`texte`);
+texte.style.color = "red";
+texte.style.fontSize = "20px";
